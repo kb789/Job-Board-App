b069fd9d68896c4267d4eb7c630c9759922f2c44
Kristina Bucher <bucher.kristina@gmail.com>
2022-09-22 18:43:02 -0400

    center button

--- pages/job/[id].js
+++ pages/job/[id].js
@@ -114,7 +114,7 @@ export default function Job({ job, app_users, app_users_ids }) {
           </a> }
       </div>
       : <div>
-         <a  onClick={() => signIn({ callbackUrl: '/api/auth/signin' })} className="mt-6 inline-block text-teal-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 text-center">SIGN IN TO APPLY</a>
+         <a  onClick={() => signIn({ callbackUrl: '/api/auth/signin' })} className="text-center mt-6 inline-block text-teal-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 text-center">SIGN IN TO APPLY</a>
       </div>)
       } 
     </div>
